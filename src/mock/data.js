73757caf5b33edf3f4f2fc0bc8746948be0b418e
Issa Makki <issamakki75@gmail.com>
2021-01-21 22:24:29 +0200

    commiting

--- src/mock/data.js
+++ src/mock/data.js
@@ -18,48 +18,74 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne: 'Highly motivated Computer Engineering student minoring in Data Analytics.',
+  paragraphTwo:
+    ' I am a Merit Scholarship grantee with a passion in Web Development and Data Science. ',
+  paragraphThree:
+    'I have also had the privilege to take on leadership positions which have cultivated my communication and soft skills.',
+  resume:
+    'https://www.linkedin.com/in/issa-makki-23bbb91a9/detail/overlay-view/urn:li:fsd_profileTreasuryMedia:(ACoAADDEDQ8BaGmDKt0wulmz0DNo-W0loLooBDg,1611243597582)/', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'keeperappphoto.jpg',
+    title: 'Google Keep Clone (React.js Front-end)',
+    info: 'Google Keep Clone (Front-end) built using React.js with icons from Material-UI!',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://issamakki7.github.io/keeperapp/',
+    repo: 'https://github.com/issamakki7/keeperapp', // if no repo, the button will not show up
+  },
+
+  {
+    id: nanoid(),
+    img: 'todolist.jpg',
+    title: 'To-Do List App',
+    info: 'To-Do List App built using React.js!',
+    info2: '',
+    url: 'https://issamakki7.github.io/todolist/',
+    repo: 'https://github.com/issamakki7/todolist', // if no repo, the button will not show up
+  },
+
+  {
+    id: nanoid(),
+    img: 'startup.jpg',
+    title: 'Start-Up Landing Page',
+    info: 'Startup Landing Page which sends the user to a Newsletter sign-up page! ',
+    info2: '',
+    url: 'https://issamakki7.github.io/startup-of-the-year/',
+    repo: 'https://github.com/issamakki7/startup-of-the-year', // if no repo, the button will not show up
+  },
+
+  {
+    id: nanoid(),
+    img: 'robofriends.jpg',
+    title: 'RoboFriends',
+    info:
+      'React.js App integrating an API that renders random robots on the website which can be filtered using the Search feature',
+    info2: '',
+    url: 'https://issamakki7.github.io/robofriends/',
+    repo: 'https://github.com/issamakki7/robofriends', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'emojipedia.jpg',
+    title: 'Emojipedia',
+    info:
+      'React.js App that displays some emojis and their corresponding meaning (Meanings derived from emojimeanings.net)',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://issamakki7.github.io/emojipedia',
+    repo: 'https://github.com/issamakki7/emojipedia', // if no repo, the button will not show up
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: 'Email me!',
+  btn: `Let's Talk`,
+  email: 'issa.makki@lau.edu',
 };
 
 // FOOTER DATA
@@ -67,28 +93,18 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/issa-makki-23bbb91a9/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/issamakki7',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
